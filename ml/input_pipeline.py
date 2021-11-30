bc891625ac4c3653239f6c0759aff6bc1d966b73
papadeiv <davidepapapicco93@gmail.com>
2021-11-30 16:32:09 +0100

    Testing input pipeline for preliminary model

---
+++ ml/input_pipeline.py
@@ -0,0 +1,105 @@
+import torch
+from torch import nn
+
+# Import model's tokenizer from HuggingFace
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("neuraly/bert-base-italian-cased-sentiment")
+
+# Show how the model's tokenizer works
+batch = ["Non credo affatto che mi andrà male", "Che bella giornata per non passare Analisi 1"]
+idx=tokenizer(batch)
+print(idx)
+
+# Read and load the dataset from files
+from pathlib import Path
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir/label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir is "neg" else 1)
+
+    return texts, labels
+
+train_texts, train_labels = read_imdb_split('aclImdb/train')
+test_texts, test_labels = read_imdb_split('aclImdb/test')
+
+# Split the training set into train and validation
+from sklearn.model_selection import train_test_split
+train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=.2)
+
+# Encode data into tensors via tokenization
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+# Transform the dataset's files into Dataset objects
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = IMDbDataset(train_encodings, train_labels)
+val_dataset = IMDbDataset(val_encodings, val_labels)
+test_dataset = IMDbDataset(test_encodings, test_labels)
+
+# Import the model itself and the Trainer API from HuggingFace
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
+model = AutoModelForSequenceClassification.from_pretrained("neuraly/bert-base-italian-cased-sentiment")
+
+# Fine tuning on the custom Dataset (object) using Trainer
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=3,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset             # evaluation dataset
+)
+trainer.train()
+
+# Fine tuning on the custom Dataset (object) using native PyTorch
+from torch.utils.data import DataLoader
+from transformers import AdamW
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+model = AutoModelForSequenceClassification.from_pretrained("neuraly/bert-base-italian-cased-sentiment")
+model.to(device)
+model.train()
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()
