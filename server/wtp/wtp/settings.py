5a18429042f4be62fac8163fd00d65141b712666
Michele Festa <48391049+mikeus9908@users.noreply.github.com>
2021-11-30 15:05:40 +0100

    Update settings.py
    removed app

--- server/wtp/wtp/settings.py
+++ server/wtp/wtp/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
