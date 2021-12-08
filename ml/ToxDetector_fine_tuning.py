e33a990d65268db26ff9c0323e22acebc0a12f1d
papadeiv <davidepapapicco93@gmail.com>
2021-12-08 11:03:00 +0100

    Pipeline for toxic detection in italian. initial database of tweets and facebook comments

---
+++ ml/ToxDetector_fine_tuning.py
@@ -0,0 +1,102 @@
+import torch
+import pandas as pd
+from torch import nn
+
+# Generate train Pandas DF (dataframe)
+df = pd.read_csv('italian/haspeede_FB-train.tsv', sep='\t')
+train_text = df.loc[:,['comment']]
+train_label = df.loc[:,['toxic']]
+
+df = pd.read_csv('italian/haspeede_TW-train.tsv', sep='\t')
+train_text = train_text.append(df.loc[:,['comment']])
+train_label = train_label.append(df.loc[:,['toxic']])
+
+train_text.dropna()
+train_label.dropna()
+#print(text.tail(10), label.tail(10))
+#print(text[1:10], label[1:10])
+#print(text.iloc[5999])
+
+# Generate test Pandas DF (dataframe)
+df = pd.read_csv('italian/haspeede_FB-test.tsv', sep='\t')
+test_text = df.iloc[:,1]
+test_label = df.iloc[:,1]
+
+df = pd.read_csv('italian/haspeede_TW-test.tsv', sep='\t')
+test_text = test_text.append(df.iloc[:,1])
+test_label = test_label.append(df.iloc[:,1])
+
+test_text.dropna()
+test_label.dropna()
+
+# Convert DF to Numpy array and create train, test and validation datasets
+from sklearn.model_selection import train_test_split
+train_x, valid_x, train_y, valid_y = train_test_split(train_text.to_numpy(), train_label.to_numpy(), test_size=.2, shuffle=True)
+test_x, test_y = test_text.to_numpy(), test_label.to_numpy()
+
+# Convert content of Numpy arrays to strings
+import numpy as np
+np.array(map(str, train_x))
+np.array(map(str, valid_x))
+np.array(map(str, test_x))
+
+#print(train_text.shape)
+#print(train_x.shape)
+#print(valid_x.shape)
+
+# Tokenize text inputs in the datasets
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("./model") # neuraly/bert-base-italian-cased-sentiment
+
+train_tkn = tokenizer(train_x.tolist(), truncation=True, padding=True, is_split_into_words=True)
+valid_tkn = tokenizer(valid_x.tolist(), truncation=True, padding=True, is_split_into_words=True)
+test_tkn = tokenizer(test_x.tolist(), truncation=True, padding=True, is_split_into_words=True)
+
+#tokenized_input = train_tkn["input_ids"][137]
+#print(tokenized_input)
+#print(tokenizer.decode(tokenized_input))
+
+# Create PyTorch Dataset objects
+class TorchSet(torch.utils.data.Dataset):
+    def __init__(self, tokens, labels):
+        self.tokens = tokens
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.tokens.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = TorchSet(train_tkn, train_y)
+valid_dataset = TorchSet(valid_tkn, valid_y)
+test_dataset = TorchSet(test_tkn, test_y)
+
+# Building pre-trained model and Trainer API
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
+model = AutoModelForSequenceClassification.from_pretrained("./model") # neuraly/bert-base-italian-cased-sentiment
+
+training_args = TrainingArguments(
+    output_dir='./ita_results',
+    num_train_epochs=3,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=16,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./ita_logs',
+    logging_steps=10,
+)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=valid_dataset
+)
+
+# Fine-tuning on custom dataset
+trainer.train()
+
+model.save_pretrained('./model')
+tokenizer.save_pretrained('./model')
---
+++ ml/ToxDetector_infere.py
@@ -0,0 +1,26 @@
+import torch
+import pandas as pd
+from torch import nn
+
+# Import fine-tuned model/tokenizer
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("./model") # neuraly/bert-base-italian-cased-sentiment
+
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
+model = AutoModelForSequenceClassification.from_pretrained("./model") # neuraly/bert-base-italian-cased-sentiment
+
+# Create and tokenize input examples
+text = ["Se non capisci sta roba al terzo anno stai messo male", "Ma lascia stare ingegneria che non fa per te", "Analisi 2 è uno schifo ma con un po di pazienza la si supera"]
+text_tkn = tokenizer(text, padding=True, truncation=True, is_split_into_words=True, return_tensors="pt")
+tokens = tokenizer.tokenize(text, padding=True, truncation=True, is_split_into_words=True, return_tensors="pt")
+
+#print(tokens)
+#for idx in text_tkn['input_ids']:
+#    print(idx)
+#    print(tokenizer.decode(idx))
+
+# Infere the inputs' labels through the model and obtain the logits
+outputs = model(**text_tkn, labels=torch.tensor([1]))
+print(outputs)
+probabilities = nn.functional.softmax(outputs.logits, dim=-1)
+print(probabilities)
