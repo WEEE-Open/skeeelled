51970280094f3d30303be631fa166584e6c70ee5
mikeus9908 <mikeus9908@gmail.com>
2021-12-15 12:58:02 +0100

    MongoDB connection with fastapi

--- server/docker/fastAPI/main.py
+++ server/docker/fastAPI/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from typing import Optional
-import uvicorn
+import motor.motor_asyncio
 
 app = FastAPI()
 
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://root:example@mongo:27017/")
+db = client.db
 
 @app.get("/")
 def index():
