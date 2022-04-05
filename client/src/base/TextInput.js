81423d050233f02bd793018c935fbc3236deea6b
Sahircan Sürmeli <ssahircan@gmail.com>
2022-04-05 17:19:21 +0200

    Add inline TeX input to TextInput

--- client/src/base/TextInput.js
+++ client/src/base/TextInput.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-import ReactMde from "react-mde";
+import ReactMde, { getDefaultToolbarCommands } from "react-mde";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import texLogo from "./textInput/TeX_logo.svg";
+
 import "react-mde/lib/styles/css/react-mde-all.css";
 import "katex/dist/katex.min.css";
+import "./textInput/textInput.css";
+
+const insertTex = {
+	name: "insert-tex",
+	icon: () => <img src={texLogo} width="20" height="20" alt="Insert TeX" />,
+	execute: ({ textApi, initialState }) => {
+		const state = textApi.replaceSelection(`$${initialState.selectedText}$`);
+		textApi.setSelectionRange({
+			start: 1,
+			end: state.selection.end - 1
+		});
+	}
+}
 
 function TextInput() {
 	const [value, setValue] = useState("");
 	const [selectedTab, setSelectedTab] = useState("write");
-
 
 	return (
 		<Container>
@@ -21,6 +35,8 @@ function TextInput() {
 				onChange={setValue}
 				selectedTab={selectedTab}
 				onTabChange={setSelectedTab}
+				commands={{ "insert-tex": insertTex }}
+				toolbarCommands={[...getDefaultToolbarCommands(), ["insert-tex"]]}
 				generateMarkdownPreview={(markdown) => 
 					Promise.resolve(
 						<ReactMarkdown
