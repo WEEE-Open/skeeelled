27a236f3fc9d4f9b685a6c15afe3859c11988cd7
papadeiv <davidepapapicco93@gmail.com>
2022-04-20 10:31:42 +0200

    Integration of language id model with toxicity detectors

--- ml/Detectors.py
+++ ml/Detectors.py
@@ -12,14 +12,21 @@ from nltk import word_tokenize
 #--------------------#
 class ProfanityDetector:
 
-  def __init__(self, some_text):
-    self.tokenized_comment = word_tokenize((str(some_text)).lower())
-    self.stopwords = ["per", "è", "é", "e", "il", "che", "tua", "sua", "un", "uno", "una"]
-    self.vocabulary = ["merda", "cazzo", "incazzato", "stocazzo", "coglione", "minchia", "minchiata", "minchiate", "stronzo", "stronzata", "stronzate", "puttana", "puttanata", "puttanate", "vaffanculo", "culo", "ricchione", "frocio", "incel"]
+  def __init__(self, language_id):
+    if language_id == 0:
+      # Italian dictionary of swear/stop-words is built
+      self.stopwords = ["per", "è", "é", "e", "il", "che", "tua", "sua", "un", "uno", "una"]
+      self.vocabulary = ["merda", "cazzo", "incazzato", "stocazzo", "coglione", "minchia", "minchiata", "minchiate", "stronzo", "stronzata", "stronzate", "puttana", "puttanata", "puttanate", "vaffanculo", "culo", "ricchione", "frocio", "incel"]
+    else:
+      # English dictionary of swear/stop-words is built
+      self.stopwords = ["per", "è", "é", "e", "il", "che", "tua", "sua", "un", "uno", "una"]
+      self.vocabulary = ["merda", "cazzo", "incazzato", "stocazzo", "coglione", "minchia", "minchiata", "minchiate", "stronzo", "stronzata", "stronzate", "puttana", "puttanata", "puttanate", "vaffanculo", "culo", "ricchione", "frocio", "incel"]
+
 
   # Check is each word in input text is present in profanity vocab
-  def purity_check(self):
-    for word in self.tokenized_comment:
+  def purity_check(self, comment):
+    tokenized_comment = word_tokenize((str(comment)).lower())
+    for word in tokenized_comment:
       # Exclude evry stopword from the actual check
       if word not in self.stopwords:
         # Swear word/Profanity is found in the input text
@@ -38,15 +45,15 @@ class ProfanityDetector:
 #-------------------#
 class ToxicityDetector:
 
-  def __init__(self, idx):
-    # List of HF hosted transformers (https://huggingface.co/models)
-    models = ["m-polignano-uniba/bert_uncased_L-12_H-768_A-12_italian_alb3rt0", #0
-              "neuraly/bert-base-italian-cased-sentiment",                      #1
-              "Hate-speech-CNERG/dehatebert-mono-italian",                      #2
-              "MilaNLProc/feel-it-italian-sentiment",                           #3
-              "dbmdz/bert-base-italian-xxl-uncased",                            #4
-              "unideeplearning/polibert_sa"]                                    #5
-
+  def __init__(self, language_id):
+    # Fine-tuned transformer models
+    models = ["Hate-speech-CNERG/dehatebert-mono-italian",
+              "martin-ha/toxic-comment-model"]
+    # STEM-oriented datasets for toxicity detection
+    datasets = ["ITA-Dataset.json",
+                "ENG-Dataset.json"]
+    # Assign language ID to model
+    self.ID = language_id
     # Assign inference hardware
     self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -55,28 +62,30 @@ class ToxicityDetector:
     #wandb.login()
     #wandb.init(project="Neuraly")
 
-    # Initialise tokeniser of the model
+    # Initialise model and dataset
     from transformers import AutoTokenizer, AutoModelForSequenceClassification
-    self.tokenizer = AutoTokenizer.from_pretrained(models[idx])
-    self.model = AutoModelForSequenceClassification.from_pretrained(models[idx])
+    self.tokenizer = AutoTokenizer.from_pretrained(models[self.ID])
+    self.model = AutoModelForSequenceClassification.from_pretrained(models[self.ID])
     self.model = self.model.to(self.device)
+    self.dataset = datasets[self.ID]
 
     # Instantiate transformer's arguments (all tunable hyperp. of the model)
     from transformers import TrainingArguments
-    self.hyperparams = TrainingArguments(output_dir='./ita_results',
+    self.hyperparams = TrainingArguments(output_dir='./train_results',
                                          num_train_epochs=3,
                                          per_device_train_batch_size=8,
                                          per_device_eval_batch_size=16,
                                          warmup_steps=500,
                                          weight_decay=0.01,
-                                         logging_dir='./ita_logs',
+                                         logging_dir='./train_logs',
                                          logging_steps=10,
                                          report_to='wandb'
                                          )
 
+
   def load_dataset(self):
     # Load full dataset and separate features from labels
-    df = pd.read_json("ITA-Dataset.json")
+    df = pd.read_json(self.dataset)
     features = df.loc[:,['text']]
     labels = df.loc[:,['sentiment']]
     # Remove NA values (if present) from features and labels
@@ -98,6 +107,7 @@ class ToxicityDetector:
     self.valid = Loader(valid_tkn, valid_y)
     self.test = Loader(test_tkn, test_y)
 
+
   def train(self):
     self.load_dataset()
     # Initialise Trainer API (https://huggingface.co/docs/transformers/training)
@@ -114,6 +124,17 @@ class ToxicityDetector:
     self.model.save_pretrained('./model')
     self.tokenizer.save_pretrained('./model')
 
+
+  def banning_policy(self, state_distribution):
+    # Extract toxicity states from labels' probability distribution
+    non_toxic, toxic = state_distribution[0,0], state_distribution[0,1]
+    # Define and enforce the bannong policy
+    if toxic > 0.5:
+      return True  # Toxic
+    else:
+      return False # Non-Toxic
+
+
   def gauge(self, comment):
     # Tokenise the input text
     tokens = self.tokenizer([comment], padding=True, truncation=True, is_split_into_words=True, return_tensors="pt").to(self.device)
@@ -121,16 +142,6 @@ class ToxicityDetector:
     outputs = self.model(**tokens)
     # Compute the labels' probability distribution
     probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)
-    return probabilities
-
-  def banning_policy(self, state_distribution):
-    # Extract toxicity states from labels' probability distribution
-    toxic, non_toxic = state_distribution[0,0], state_distribution[0,2]
-    uncertainty = state_distribution[0,1]
-    # Define and enforce the bannong policy
-    if toxic > 0.5 and uncertainty > 0.0:
-      return 0 # Negative
-    elif non_toxic > 0.5 and uncertainty > 0.0:
-      return 2 # Positive
-    else:
-      return 1 # Neutral
+    # Invoke banning policy and return quarantine score
+    score = self.banning_policy(probabilities)
+    return score
--- ml/Modali.py
+++ ml/Modali.py
@@ -1,13 +1,15 @@
-########################################################################
-########################################################################
-##                                                                    ##
-##                               MODALI                               ##
-##            MODeratore Automatico per la Lingua Italiana            ##
-##                                                                    ##
-########################################################################
-########################################################################
+##################################################################################
+##################################################################################
+##                                                                              ##
+##                                    MODALII                                   ##
+##            MODeratore Automatico per la Lingua Italiana & Inglese            ##
+##                                                                              ##
+##################################################################################
+##################################################################################
 
 import os
+import numpy as np
+from torch import nn
 
 #--------------------#
 # Trainining/testing #
@@ -21,7 +23,7 @@ def train():
 
   else:
     # Check if dataset exists
-    if os.path.exists('ITA-Dataset.json'):
+    if os.path.exists('ITA-Dataset.json') or os.path.exists('ENG-Dataset.json'):
       pass
 
     else:
@@ -41,6 +43,64 @@ def train():
     moderator.train()
 
 
+#-------------------------#
+# Language identification #
+#        pipeline         #
+#-------------------------#
+class Polyglot:
+
+  def __init__(self):
+    # Select between local and hosted pre-trained models
+    from transformers import AutoTokenizer, AutoModelForSequenceClassification
+    if os.path.exists('LangID'):
+      self.tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+      self.model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+    else:
+      self.tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+      self.tokenizer.save_pretrained('./LangID')
+      self.model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+      self.model.save_pretrained('./LangID')
+
+  def identification(self, probabilities):
+    # First compute argmax of the distribution
+    argmax = np.where(probabilities == np.amax(probabilities, axis=-1))
+    # argmax is a tuple of arrays from which we extract the label
+    language_id = argmax[0][0]
+    # Return the argmax label if it is either italian or english
+    if language_id == 5 or language_id == 13:
+      return language_id
+    # Compare the standard error of english's label w.r.t. the argmax of the distribution
+    else:
+      std_err = (probabilities[language_id] - probabilities[13])/probabilities[language_id]
+      # Lastly check whether the english label is indeed the second most probable in the PMF
+      if std_err < 0.95:
+        filtered_probabilities = np.delete(probabilities, argmax)
+        filtered_argmax = np.where(filtered_probabilities == np.amax(filtered_probabilities, axis=-1))
+        # Return the english label accordingly
+        if language_id < 13:
+          if filtered_argmax[0][0] == 12:
+            return 13
+          else:
+            return None
+        else:
+          if filtered_argmax[0][0] == 13:
+            return 13
+          else:
+            return None
+
+
+  def discern(self, input_text):
+    # Tokenise the input text
+    tokens = self.tokenizer([input_text], padding=True, truncation=True, is_split_into_words=True, return_tensors="pt")
+    # Obtain the logits of the input text
+    outputs = self.model(**tokens)
+    # Compute labels' probability distribution from logits
+    probabilities = nn.functional.softmax(outputs.logits, dim=-1).detach().numpy().reshape((-1,))
+    # Retrieve language ID from probability distribution
+    lang_id = self.identification(probabilities)
+    return lang_id
+
+
 #---------------------#
 # Full input pipeline #
 #---------------------#
@@ -49,19 +109,55 @@ def infer(comment):
   # Perform training if model has not been fine-tuned
   train()
 
-  # Check presence of swear-words/profanity in the comment
-  from Detectors import ProfanityDetector
-  detector = ProfanityDetector(comment)
+  # Filter unwanted characters out of the input text
+  sample = comment.replace("[", "").replace("]", "").replace('\\n', ' ').replace('\\', ' ').replace("'", ""). replace('"', '')
+
+  # Initialise score for swear-words reference (True=ban, False=pass)
+  score = True
+
+  # Discern language of the comment between english and italian...
+  Linguist = Polyglot()
+  language_id = Linguist.discern(sample)
+
+  # ... and instantiate sentiment analysis model accordingly!
+  if language_id == 5:
+    print("Italian language identified")
+
+    # Instantiate BoW model for swear-words/profanity
+    from Detectors import ProfanityDetector
+    detector = ProfanityDetector(0)
+
+    # Check abscence of profanity in the comment
+    if detector.purity_check(sample):
+      # Instantiate toxicity-detection model
+      from Detectors import ToxicityDetector
+      moderator = ToxicityDetector(0)
+      # Infer toxicity in the comment
+      score = moderator.gauge(sample)
+
+  elif language_id == 13:
+    print("English language identified")
+    
+    # Instantiate BoW model for swear-words/profanity
+    from Detectors import ProfanityDetector
+    detector = ProfanityDetector(1)
+
+    # Check abscence of profanity in the comment
+    if detector.purity_check(sample):
+      # Instantiate toxicity-detection model
+      from Detectors import ToxicityDetector
+      moderator = ToxicityDetector(1)
+      # Infer toxicity in the comment
+      score = moderator.gauge(sample)
   
-  # Initialise score for swer-words reference
-  score = 0
-  # Check toxicity level in the comment
-  if detector.purity_check():
-    # Check toxicity level in the comment
-    from Detectors import ToxicityDetector
-    moderator = ToxicityDetector(1)
-    distribution = moderator.gauge(comment)
-    # Invoke policy to quarantine the comment or not
-    score = moderator.banning_policy(distribution)
+  else:
+    print("Neither Italian nor English language has been identified")
+  
+  
+  # Execute ban according to the sentiment scored
+  if score:
+    print("Toxicity/Profanity has been detected\nComment has been quarantined!")
+  else:
+    print("Comment has been published!")
 
   return score
