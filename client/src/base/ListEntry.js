1d9e4cf09f51e7e378190a4feb4bc706251f63ff
e-caste-mbp <git@caste.dev>
2022-04-20 19:55:04 +0200

    fix double import line that would prevent the build

--- client/src/base/ListEntry.js
+++ client/src/base/ListEntry.js
@@ -1,4 +1,3 @@
-import { Row, Col, Image } from "react-bootstrap";
 import { Row, Col, Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./ListEntry.css";
