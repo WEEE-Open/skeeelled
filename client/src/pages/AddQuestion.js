d7eea064654b65332f9f52e4a61bb9985c481a8f
donatomodugno <donato.modugno@gmail.com>
2022-05-11 18:15:07 +0200

    AddQuestion page improved

--- client/src/pages/AddQuestion.js
+++ client/src/pages/AddQuestion.js
@@ -18,8 +18,8 @@ function AddQuestion(props) {
     <Form.Label size="lg">Choose moodle XML file</Form.Label>
     <Form.Control type="file" size="lg" />
     <List scope="selection" title="Select course" rows={courses} />
-    <Form.Check type="checkbox" label="Exam" />
-    <TextInput/>
+    <Form.Check type="checkbox" label="This is an exam simulation" />
+    {/* <TextInput/> */}
     <Button type="submit" className="submit-button my-4" onClick={() => {}}>Upload</Button>
   </>);
 }
