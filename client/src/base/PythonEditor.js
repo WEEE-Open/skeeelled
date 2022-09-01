eac5c9c32d60ea7a0d3812a71da3540d1bb55828
Sahircan Sürmeli <ssahircan@gmail.com>
2022-09-02 00:40:49 +0200

    Add micropip and make cosmetic changes to PythonEditor

--- client/src/base/PythonEditor.js
+++ client/src/base/PythonEditor.js
@@ -2,13 +2,10 @@ import React, { useState } from "react";
 import { PythonEditor } from "react-python-editor";
 
 export default function Python() {
-  const [value, setValue] = useState("");
   return (
     <PythonEditor
       editorHeight="400px"
       outputHeight="200px"
-      value={value}
-      onChange={setValue}
     />
   );
 }
