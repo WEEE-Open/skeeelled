bbe9cd1f6a27b0e51af8c14d66f086fa1f076b2a
Sahircan Sürmeli <ssahircan@gmail.com>
2022-11-18 18:21:54 +0100

    New data structure

--- server/src/models/db/simulation.py
+++ server/src/models/db/simulation.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from typing import List
 from models.objectid import ObjectId
+from datetime import datetime
 
 class ExamSimulation(BaseModel):
     course_info: ObjectId
-    timestamp = float
+    timestamp: float = datetime.now()
+    endtime: datetime = None
     content: List
     # points of a question should be fraction correct * default_grade - fraction wrong - penalty_grade
     results: List[float]
