ff7adc287d063d5cfe0dae77502643bcf3b33b51
Jim <jimmao25@gmail.com>
2023-04-14 14:39:44 +0200

    ReactMarkdown plugin added

--- client/src/base/MarkdownPreview.js
+++ client/src/base/MarkdownPreview.js
@@ -4,17 +4,17 @@ import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypeHighlight from "rehype-highlight";
+import rehypeRaw from "rehype-raw";
 
 import "./MarkdownPreview.css";
 
 const MarkdownPreview = React.memo(({ markdown, format }) => {
 
-  console.log(format)
   return (
     <ReactMarkdown
       className="markdown-preview"
       remarkPlugins={[remarkGfm, remarkMath]}
-      rehypePlugins={[rehypeKatex, rehypeHighlight]}
+      rehypePlugins={[rehypeKatex, rehypeHighlight, rehypeRaw]}
     >
       {markdown}
     </ReactMarkdown>
