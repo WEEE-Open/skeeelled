4a4d854109a2893108e51b6f382031c6afa57596
Sahircan Sürmeli <ssahircan@gmail.com>
2023-05-04 14:59:12 +0200

    Make courses requests from the page

--- client/src/GlobalStateProvider.js
+++ client/src/GlobalStateProvider.js
@@ -12,37 +12,18 @@ const GlobalStateProvider = ({ children }) => {
 
   const [mySimulationResult, setMySimulationResult] = useState([]);
 
-  const [relatedCourses, setRelatedCourses] = useState([]);
-
-  const [allCourses, setAllCourses] = useState([]);
-
-  const [userCourses, setUserCourses] = useState([]);
-
   useEffect(() => {
     console.log("state provider user id", userID);
-    API.getCourses().then((courses) => setAllCourses(courses));
     API.getMyBookmarkedQuestions(userID).then((questions) => {
       setMyBookmarkedQuestions(questions["myBookmarkedQuestions"]);
     });
     API.getUser(userID).then((info) => {
       setUserInfo(info);
-      setRelatedCourses(info["related_courses"]);
     });
     API.getMySimulationResult(userID).then((result) => {
       setMySimulationResult(result);
     });
   }, [userID]);
-
-  useEffect(() => {
-    const Courses = allCourses?.filter((course) => {
-      for (let i = 0; i < relatedCourses.length; i++) {
-        if (course["_id"] === relatedCourses[i]) {
-          return course;
-        }
-      }
-    });
-    setUserCourses(Courses);
-  }, [allCourses, relatedCourses]);
 
   return (
     <GlobalStateContext.Provider
@@ -54,10 +35,6 @@ const GlobalStateProvider = ({ children }) => {
         setMyBookmarkedQuestions,
         mySimulationResult,
         setMySimulationResult,
-        userCourses,
-        setUserCourses,
-        allCourses,
-        setAllCourses,
       }}
     >
       {children}
