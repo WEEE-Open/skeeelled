be0a47c82572fb531ce4c8d07d5dc06e1f15b23b
Sahircan Sürmeli <ssahircan@gmail.com>
2023-05-04 19:59:12 +0200

    Add MathJax support to display formulas in HTML

--- client/src/base/MarkdownPreview.js
+++ client/src/base/MarkdownPreview.js
@@ -5,13 +5,21 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypeHighlight from "rehype-highlight";
 import parse from 'html-react-parser';
+import { MathJax } from "better-react-mathjax";
 
 import "./MarkdownPreview.css";
 
+const htmlStyles = {
+  display: "block",
+};
+
 const MarkdownPreview = React.memo(({ text, format }) => {
   return format === "html" 
-  ? (parse(text))
-  : (
+  ? (
+      <MathJax style={htmlStyles}>
+        {parse(text)}
+      </MathJax>
+  ) : (
     <ReactMarkdown
       className="markdown-preview"
       remarkPlugins={[remarkGfm, remarkMath]}
