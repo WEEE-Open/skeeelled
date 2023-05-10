13758e6b7995b39c04972fa6a93f18299515761d
Jim <jimmao25@gmail.com>
2023-05-10 10:06:32 +0200

    fix questionPreview rendering issue

--- client/src/base/QuestionPreview.js
+++ client/src/base/QuestionPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import MarkdownPreview from "./MarkdownPreview";
 import "./stylesheet/QuestionPreview.css";
 import remarkGfm from "remark-gfm";
@@ -13,16 +13,21 @@ import "highlight.js/styles/github.css";
 
 function QuestionPreview({ question, showhints }) {
   const [showAdvice, setShowAdvice] = useState(showhints || false);
+  const [questionPreviewed, setQuestionPreviewed] = useState(question[0])
 
-  return (
+  useEffect(()=> {
+    setQuestionPreviewed(question[0])
+  }, [question])
+
+  return questionPreviewed && questionPreviewed._id? (
     <Card>
       <Card.Header>
         <div className="d-flex justify-content-between">
-          <div className="course-name">{`${question} ${question.name}`}</div>
+          <div className="course-name">{`${questionPreviewed._id} ${questionPreviewed.name}`}</div>
           <div className="p2">
             <small className="question-create-date">
               {
-                "Posted on " + question.timestamp
+                // "Posted on " + questionPreviewed.timestamp
                 // question.date.toLocaleDateString("it-IT", {
                 //   year: "numeric",
                 //   month: "numeric",
@@ -32,30 +37,30 @@ function QuestionPreview({ question, showhints }) {
             </small>
             <br />
             <small className="question-source">
-              by {question.owner} &nbsp;
+              by {questionPreviewed.owner.username} &nbsp;
             </small>
           </div>
         </div>
       </Card.Header>
       <Card.Body>
         <Card.Title>{question.title}</Card.Title>
-        <MarkdownPreview markdown={question.content} />
-        {question.hint && (
-          <Button
-            className="show-advice-button"
-            onClick={() => {
-              setShowAdvice(!showAdvice);
-            }}
-          >
-            {showAdvice ? "Hide advice" : "Show advice"}
-          </Button>
-        )}
-        {showAdvice && (
-          <h6 className="question-advice-shown">{question.hint}</h6>
-        )}
+        <MarkdownPreview markdown={questionPreviewed.questiontext.text} />
+        {/*{question.hint && (*/}
+        {/*  <Button*/}
+        {/*    className="show-advice-button"*/}
+        {/*    onClick={() => {*/}
+        {/*      setShowAdvice(!showAdvice);*/}
+        {/*    }}*/}
+        {/*  >*/}
+        {/*    {showAdvice ? "Hide advice" : "Show advice"}*/}
+        {/*  </Button>*/}
+        {/*)}*/}
+        {/*{showAdvice && (*/}
+        {/*  <h6 className="question-advice-shown">{question.hint}</h6>*/}
+        {/*)}*/}
       </Card.Body>
     </Card>
-  );
+  ): <></>
 }
 
 export default QuestionPreview;
